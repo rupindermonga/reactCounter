306b870c345d96536289e9c5ffdf290d4b0e3302
Rupinder Monga <rupindermonga@gmail.com>
2019-09-02 11:37:16 +0530

    Increment counter created

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
-
 class Counter extends Component {
   state = {
     count: 0
+  };
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
   };
 
   render() {
     return (
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </React.Fragment>
     );
   }
